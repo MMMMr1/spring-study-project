19684c05842e07b19e78ab76d23228f332a8ded7
Michalenok Marina <mihalenokee@outlook.com>
2023-04-28 13:35:57 +0300

    feat: @InjectBean, InjectBeanPostProcessor

--- src/main/java/com/example/database/repository/CompanyRepository.java
+++ src/main/java/com/example/database/repository/CompanyRepository.java
@@ -1,14 +1,9 @@
 package com.example.database.repository;
 
+import com.example.beanpostprocesssor.InjectBean;
 import com.example.database.pool.ConnectionPool;
 
 public class CompanyRepository {
-    private final ConnectionPool connectionPool;
-
-    private CompanyRepository(ConnectionPool connectionPool) {
-        this.connectionPool = connectionPool;
-    }
-    public static CompanyRepository of(ConnectionPool connectionPool){
-        return new CompanyRepository(connectionPool);
-    }
-}
+    @InjectBean
+    private  ConnectionPool connectionPool;
+ }
