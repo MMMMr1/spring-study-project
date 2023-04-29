16ca69b9f6af442bf410433d1edea613d01ba235
Michalenok Marina <mihalenokee@outlook.com>
2023-04-29 11:57:42 +0300

    feat: ApplicationConfiguration

--- src/main/java/com/example/ApplicationRunner.java
+++ src/main/java/com/example/ApplicationRunner.java
@@ -1,13 +1,15 @@
 package com.example;
 
+import com.example.config.ApplicationConfiguration;
 import com.example.database.pool.ConnectionPool;
 import com.example.database.repository.CrudRepository;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
 
 	public static void main(String[] args) {
-		try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml")) {
+		try (var context = new AnnotationConfigApplicationContext(ApplicationConfiguration.class)) {
 
 			ConnectionPool pool1 = context.getBean("pool1", ConnectionPool.class);
 			System.out.println(pool1);
