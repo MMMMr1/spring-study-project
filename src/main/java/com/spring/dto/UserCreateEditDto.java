19670a292615a1cb09fa258a637fc19c457d76b6
Michalenok Marina <mihalenokee@outlook.com>
2023-05-22 10:37:29 +0300

    feat: parsing LocalDate (using Jsr310Converters)

--- src/main/java/com/spring/dto/UserCreateEditDto.java
+++ src/main/java/com/spring/dto/UserCreateEditDto.java
@@ -1,11 +1,8 @@
 package com.spring.dto;
 
-import com.spring.database.entity.Company;
 import com.spring.database.entity.Role;
-import jakarta.persistence.*;
 import lombok.Value;
 import lombok.experimental.FieldNameConstants;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -13,7 +10,7 @@ import java.time.LocalDate;
 @FieldNameConstants
 public class UserCreateEditDto {
     String username;
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
+//    @DateTimeFormat(pattern = "yyyy-MM-dd")
     LocalDate birthDate;
     String firstname;
     String lastname;
