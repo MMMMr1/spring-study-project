c3ebd735d4a2b7a6c26c7ad8764df90a391f095d
Michalenok Marina <mihalenokee@outlook.com>
2023-06-20 21:51:46 +0300

    feat:  authorization (requestMatchers)

--- src/main/java/com/spring/config/SecurityConfiguration.java
+++ src/main/java/com/spring/config/SecurityConfiguration.java
@@ -2,18 +2,30 @@ package com.spring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static com.spring.database.entity.Role.ADMIN;
+
 @Configuration
 public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeHttpRequests().anyRequest().authenticated();
+        http.authorizeHttpRequests(urlConfig ->urlConfig
+                .requestMatchers("/login").permitAll()
+                .requestMatchers("/users/registration").permitAll()
+                .requestMatchers("/v3/api-docs/**").permitAll()
+                .requestMatchers("/swagger-ui/**").permitAll()
+
+                .requestMatchers( "/users/{id:\\d+}/delete").hasAuthority(ADMIN.getAuthority())
+                .requestMatchers("/admin/**").hasAuthority(ADMIN.getAuthority())
+                .anyRequest().authenticated()
+        ) ;
 //        http.httpBasic(Customizer.withDefaults());
         http.logout(logout -> logout
                 .logoutUrl("/logout")
